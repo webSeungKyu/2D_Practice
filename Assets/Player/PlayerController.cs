822961a12260460f46c331394d2fa3b622173ed4
webSeungKyu <abcsksk1@gmail.com>
2024-03-15 23:48:43 +0900

    gameClear와 gameOver 구현

--- Assets/Player/PlayerController.cs
+++ Assets/Player/PlayerController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -12,12 +13,27 @@ public class PlayerController : MonoBehaviour
     public LayerMask groundLayer; // 착지할 수 있는 레이어
     bool goJump = false; // 점프 개시 플래그
     bool onGround = false; // 지면에 서 있는 플래그
+
+    //애니메이션 처리
+    Animator animator; // 애니메이터
+    public string stopAnime = "PlayerStop";
+    public string moveAnime = "PlayerMove";
+    public string jumpAnime = "PlayerJump";
+    public string goalAnime = "PlayerGoal";
+    public string deadAnime = "PlayerOver";
+    string nowAnime = "";
+    string oldAnime = "";
+    public static string gameState;
     #endregion
     // Start is called before the first frame update
     void Start()
     {
         //Rigidbody2D 가져오기
         rbody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        nowAnime = stopAnime;
+        oldAnime = stopAnime;
+        gameState = "playing";
     }
 
     // Update is called once per frame
@@ -39,10 +55,19 @@ public class PlayerController : MonoBehaviour
         {
             Jump();
         }
+        if(gameState != "playing")
+        {
+            return;
+        }
     }
 
     void FixedUpdate()
     {
+        if(gameState != "playing")
+        {
+            return;
+        }
+
         //착지 판정
         //Physics2D.Linecast는 두 점을 연결하는 선에 오브젝트가 접촉하는지 조사해 true 혹은 false로 반환함
         //transform.up은 백터로 x = 0, y = 1, z = 0으로 나타냄
@@ -62,10 +87,71 @@ public class PlayerController : MonoBehaviour
             goJump = false;
         }
 
+        if (onGround)
+        {
+            //지면 위일 경우
+            if(axisH == 0)
+            {
+                nowAnime = stopAnime;
+            }
+            else
+            {
+                nowAnime = moveAnime;
+            }
+        }
+        else
+        {
+            //공중일 경우
+            nowAnime = jumpAnime;
+        }
+
+        if(nowAnime != oldAnime)
+        {
+            oldAnime = nowAnime;
+            animator.Play(nowAnime);
+        }
+
     }
 
     public void Jump()
     {
         goJump = true;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Goal")
+        {
+            Goal();
+        }
+        else if (collision.gameObject.tag == "Dead")
+        {
+            GameOver();
+        }
+    }
+
+    public void Goal()
+    {
+        animator.Play(goalAnime);
+        gameState = "gameClear";
+        GameStop();
+    }
+    
+    public void GameOver()
+    {
+        animator.Play(deadAnime);
+
+        gameState = "gameOver";
+        GameStop();
+
+        GetComponent<CapsuleCollider2D>().enabled = false; //플레이어 충돌 비활성화
+        rbody.AddForce(new Vector2(0, 5), ForceMode2D.Impulse);
+    }
+
+    public void GameStop()
+    {
+        
+        rbody.velocity = new Vector2(0, 0);
+        transform.position = GameObject.FindGameObjectWithTag("Goal").GetComponent<Transform>().position;
     }
 }
